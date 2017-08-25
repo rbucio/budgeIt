d7e610b1f0f014f3f31153d22c609293a30532a5
Rey <rbuciodev@gmail.com>
2017-08-24 22:22:56 -0700

    added event listener to add button when click or press enter

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
+// BUDGET CONTROLLER
+var budgetController = (function() {
 
+
+
+})();
+
+
+// UI CONTROLLER
+var UIController = (function() {
+
+
+
+})();
+
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
+
+    var ctrlAddItem = function() {
+
+        //1. Get input data
+
+        //2. Add the item to the budget controller
+
+        //3. Add the item to the UI
+
+        //4. Calculate the budget
+
+        //5. Display the budget on the UI
+        console.log('It works');
+    }
+
+    document.querySelector('#add-button').addEventListener('click', ctrlAddItem);
+
+    window.addEventListener('keypress', function(e) {
+
+        if (e.keyCode === 13 || e.which === 13) {
+            ctrlAddItem();
+        }
+
+    });
+
+
+
+})( budgetController, UIController);
