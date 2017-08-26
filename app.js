ee1141f61bf1b8eedbb88418532b723880711dee
Rey <rbuciodev@gmail.com>
2017-08-26 16:48:06 -0700

    formatted strings before displaying to UI

--- app.js
+++ app.js
@@ -113,6 +113,36 @@ var UIController = (function() {
         container: '#display-list-container'
     }
 
+    var formatNumber = function(num, type) {
+
+        var numSplit, int, dec;
+
+        /*
+        + or - before number
+        exactly 2 decimal points
+        comma separating the thousands
+
+        2310.4567 -> + 2,310.46
+        */
+
+        num = Math.abs(num);
+        num = num.toFixed(2);
+
+        numSplit = num.split('.');
+
+        int= numSplit[0];
+        if(int.length > 3) {
+            int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, 3);
+        }
+
+        dec = numSplit[1];
+
+        type === 'exp' ? sign = '-' : sign = '+';
+
+        return sign + ' ' + int + '.' + dec;
+
+    }
+
     return {
         getInput: function() {
             return {
@@ -143,7 +173,7 @@ var UIController = (function() {
             // Replace the placeholder text with the actual data
             newHTML = html.replace('%id%', obj.id);
             newHTML = newHTML.replace('%description%', obj.description);
-            newHTML = newHTML.replace('%amount%', obj.amount);
+            newHTML = newHTML.replace('%amount%', formatNumber(obj.amount, type));
 
             // Insert the HTML into the DOM
             document.querySelector(container).insertAdjacentHTML('beforeend', newHTML);
@@ -171,9 +201,13 @@ var UIController = (function() {
 
         },
         displayBudget: function(obj) {
-            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
-            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
-            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+            var type;
+            obj.budget > 0 ? type = 'inc' : type = 'exp';
+
+            document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget, type);
+            document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.budget, 'inc');
+            document.querySelector(DOMstrings.expensesLabel).textContent = formatNumber(obj.budget, 'exp');
         },
         displayDate: function() {
             var now, months, month, year;
